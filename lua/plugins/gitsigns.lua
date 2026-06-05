9c16a70ba6af4291a68e4ae01e6e5700cbe8145a
HMasataka <masataka.hisasue@gmail.com>
2026-06-05 22:20:10 +0900

    gitsignにunstage追加

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -15,8 +15,9 @@ return {
 
             map("n", "<leader>hs", gs.stage_hunk, "Stage hunk")
             map("n", "<leader>hr", gs.reset_hunk, "Reset hunk")
-            map("v", "<leader>hs", function() gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") }) end, "Stage selection")
-            map("v", "<leader>hr", function() gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") }) end, "Reset selection")
+            map("n", "<leader>hu", gs.undo_stage_hunk, "Undo stage hunk")
+            map("v", "<leader>hs", "<cmd>Gitsigns stage_hunk<CR>", "Stage selection")
+            map("v", "<leader>hr", "<cmd>Gitsigns reset_hunk<CR>", "Reset selection")
             map("n", "<leader>hp", gs.preview_hunk, "Preview hunk")
             map("n", "<leader>hb", function() gs.blame_line({ full = true }) end, "Blame line")
         end,
